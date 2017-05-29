d07672b740a10b50971eb0ddb8c965f4a7ae1663
Andrew Alexander <bdrewsands@gmail.com>
2017-05-29 20:23:22 +0700

    Modification to web

--- blog/views.py
+++ blog/views.py
@@ -26,6 +26,9 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+def contact(request):
+    return render(request, 'blog/contact.html');
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
